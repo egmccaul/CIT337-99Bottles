14fd23ed5da2a2f68067e3dfe8f033b5fff436f2
Emily McCauley <egmccaul@oakland.edu>
2016-02-02 08:17:05 -0500

    Added comments to code to explain what is occurring.

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -4,14 +4,21 @@
 public class BeerSong {
 
     public static void main(String args[]){
+
+        // Calls the method that will output the Beer Song.
         Ninety_Nine_Bottles_of_Beer();
     }
 
     public static void Ninety_Nine_Bottles_of_Beer(){
+
+        // Creates a loop that counts down from 99.
+        // In this loop, it will output the verses of the Beer Song until it reaches 1 bottle
         for (int i=99; i > 1; i--){
             System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer");
             System.out.println("take one down, pass it around, " + (i-1) +" bottles of beer on the wall.");
         }
+
+        // Once there it one bottle left the song will change to output below.
         System.out.println("1 bottle of	beer on	the	wall, 1	bottle of beer");
         System.out.println("take one down, pass	it around, no more bottles of beer on the wall.");
     }
