6ac471ba81bfa5a3178d5ef33562469bc8b13acc
Emily McCauley <egmccaul@oakland.edu>
2016-02-02 07:58:05 -0500

    Implemented the use of Ninety_Nine_Bottles_of_Beer().

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -4,15 +4,15 @@
 public class BeerSong {
 
     public static void main(String args[]){
-
+        Ninety_Nine_Bottles_of_Beer();
     }
 
-    public void Ninety_Nine_Bottles_of_Beer(){
+    public static void Ninety_Nine_Bottles_of_Beer(){
         for (int i=99; i > 1; i--){
-            System.println(i + " bottles of	beer on	the	wall, " + i + " bottles	of beer");
-            System.println("take one down, pass	it around, " + (i-1) +"	bottles	of beer	on the wall.");
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer");
+            System.out.println("take one down, pass it around, " + (i-1) +" bottles of beer on the wall.");
         }
-        System.println("1 bottle of	beer on	the	wall, 1	bottle of beer");
-        System.println("take one down, pass	it around, no more bottles of beer on the wall.");
+        System.out.println("1 bottle of	beer on	the	wall, 1	bottle of beer");
+        System.out.println("take one down, pass	it around, no more bottles of beer on the wall.");
     }
 }
